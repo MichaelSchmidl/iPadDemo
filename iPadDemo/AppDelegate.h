40995e73d0ce5de1d8fb73c2e79d5839cb569a32
Michael Schmidl <Michael.Schmidl@sysoc.de>
2014-09-23 19:40:32 +0200

    Initial Commit

---
+++ iPadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPadDemo
+//
+//  Created by Michael Schmidl on 23.09.14.
+//  Copyright (c) 2014 Michael Schmidl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
